a5e8b784a01c4a0a81fc7f88b8f1b225c26fe3da
ezio84-xda <brabus84@gmail.com>
2014-09-26 17:55:24 +0200

    cyanogen sqlite fetching
    Change-Id: Ie8da4eac663e1e6e64ca4ff288de4b63e02601e1

--- fetch-merge-slimroms.sh
+++ fetch-merge-slimroms.sh
@@ -41,6 +41,7 @@ echo -e "${bldgrn}Updating hardware/qcom/display ${txtrst}"
 cd $rdir/hardware/qcom/display && git fetch slimroms && git merge slimroms/kk4.4
 echo -e "${bldgrn}Updating vendor/slim ${txtrst}"
 cd $rdir/vendor/slim && git fetch slimroms && git merge slimroms/kk4.4
-
+echo -e "${bldgrn}Updating external/sqlite ${txtrst}"
+cd $rdir/external/sqlite && git fetch cm && git merge cm/cm-11.0
 # The end
 echo -e "${bldgrn}Done, exiting ${txtrst}"
