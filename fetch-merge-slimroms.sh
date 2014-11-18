a78520b6fa0344469a79ced72cf896f0687488f2
ezio84-xda <brabus84@gmail.com>
2014-11-18 12:21:59 +0100

    push github instead of push http://..., thanks to git remote set-url
    Change-Id: I992c337f3d9f70dfbf16e4d49bbd8837d4c3303d

--- fetch-merge-slimroms.sh
+++ fetch-merge-slimroms.sh
@@ -12,18 +12,18 @@ echo -e "${bldgrn}Run this script from the root repo folder! ${txtrst}"
 
 #`check for updates and merge
 echo -e "${bldgrn}Updating frameworks/base ${txtrst}"
-cd $rdir/frameworks/base && git fetch slimroms && git merge slimroms/lp5.0 && git push https://github.com/ezio84/frameworks_base lp5.0
+cd $rdir/frameworks/base && git fetch slimroms && git merge slimroms/lp5.0 && git push github lp5.0
 echo -e "${bldgrn}Updating packages/apps/Settings ${txtrst}"
-cd $rdir/packages/apps/Settings && git fetch slimroms && git merge slimroms/lp5.0 && git push https://github.com/ezio84/packages_apps_Settings lp5.0
+cd $rdir/packages/apps/Settings && git fetch slimroms && git merge slimroms/lp5.0 && git push github lp5.0
 echo -e "${bldgrn}Updating build ${txtrst}"
-cd $rdir/build && git fetch slimroms && git merge slimroms/lp5.0 && git push https://github.com/ezio84/android_build lp5.0
+cd $rdir/build && git fetch slimroms && git merge slimroms/lp5.0 && git push github lp5.0
 echo -e "${bldgrn}Updating vendor/slim ${txtrst}"
-cd $rdir/vendor/slim && git fetch slimroms && git merge slimroms/lp5.0 && git push https://github.com/ezio84/vendor_slim lp5.0
+cd $rdir/vendor/slim && git fetch slimroms && git merge slimroms/lp5.0 && git push github lp5.0
 echo -e "${bldgrn}Updating packages/apps/SlimLauncher ${txtrst}"
-cd $rdir/packages/apps/SlimLauncher && git fetch slimroms && git merge slimroms/lp5.0 && git push https://github.com/ezio84/packages_apps_SlimLauncher lp5.0
+cd $rdir/packages/apps/SlimLauncher && git fetch slimroms && git merge slimroms/lp5.0 && git push github lp5.0
 echo -e "${bldgrn}Updating packages/apps/SlimCenter ${txtrst}"
-cd $rdir/packages/apps/SlimCenter && git fetch slimroms && git merge slimroms/lp5.0 && git push https://github.com/ezio84/packages_apps_SlimCenter lp5.0
+cd $rdir/packages/apps/SlimCenter && git fetch slimroms && git merge slimroms/lp5.0 && git push github lp5.0
 echo -e "${bldgrn}Updating packages/apps/SlimFileManager ${txtrst}"
-cd $rdir/packages/apps/SlimFileManager && git fetch slimroms && git merge slimroms/lp5.0 && git push https://github.com/ezio84/packages_apps_SlimFileManager lp5.0
+cd $rdir/packages/apps/SlimFileManager && git fetch slimroms && git merge slimroms/lp5.0 && git push github lp5.0
 # The end
 echo -e "${bldgrn}Done, exiting ${txtrst}"
