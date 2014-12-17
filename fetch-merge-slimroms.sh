1fd16618fa7f33731d09a38bb66b7ea6528261bb
ezio84-xda <brabus84@gmail.com>
2014-12-17 14:10:12 +0100

    Update fetch merge script with Audiofx/11/audiovisualizer and
    remove Slimlauncher/SlimFilemanager repos (now that translations are
    on Crowdin)
    
    Change-Id: I660321c540c54cb5bc0a268908af9e2dfca7ccc6

--- fetch-merge-slimroms.sh
+++ fetch-merge-slimroms.sh
@@ -19,12 +19,8 @@ echo -e "${bldgrn}Updating build ${txtrst}"
 cd $rdir/build && git fetch slimroms && git merge slimroms/lp5.0 && git push https://github.com/ezio84/android_build lp5.0
 echo -e "${bldgrn}Updating vendor/slim ${txtrst}"
 cd $rdir/vendor/slim && git fetch slimroms && git merge slimroms/lp5.0 && git push https://github.com/ezio84/vendor_slim lp5.0
-echo -e "${bldgrn}Updating packages/apps/SlimLauncher ${txtrst}"
-cd $rdir/packages/apps/SlimLauncher && git fetch slimroms && git merge slimroms/lp5.0 && git push https://github.com/ezio84/packages_apps_SlimLauncher lp5.0
 echo -e "${bldgrn}Updating packages/apps/SlimCenter ${txtrst}"
 cd $rdir/packages/apps/SlimCenter && git fetch slimroms && git merge slimroms/lp5.0 && git push https://github.com/ezio84/packages_apps_SlimCenter lp5.0
-echo -e "${bldgrn}Updating packages/apps/SlimFileManager ${txtrst}"
-cd $rdir/packages/apps/SlimFileManager && git fetch slimroms && git merge slimroms/lp5.0 && git push https://github.com/ezio84/packages_apps_SlimFileManager lp5.0
 echo -e "${bldgrn}Updating device/lge/mako ${txtrst}"
 cd $rdir/device/lge/mako && git fetch slimroms && git merge slimroms/lp5.0 && git push https://github.com/ezio84/device_lge_mako lp5.0
 echo -e "${bldgrn}Updating kernel/lge/mako ${txtrst}"
@@ -33,5 +29,11 @@ echo -e "${bldgrn}Updating device/lge/hammerhead ${txtrst}"
 cd $rdir/device/lge/hammerhead && git fetch slimroms && git merge slimroms/lp5.0 && git push https://github.com/ezio84/device_lge_hammerhead lp5.0
 echo -e "${bldgrn}Updating kernel/lge/hammerhead ${txtrst}"
 cd $rdir/kernel/lge/hammerhead && git fetch slimroms && git merge slimroms/lp5.0 && git push https://github.com/ezio84/kernel_lge_hammerhead lp5.0
+echo -e "${bldgrn}Updating AudioVisualizer by Cyanogen ${txtrst}"
+cd $rdir/external/android-visualizer && git fetch cm && git merge cm/cm-12.0 && git push https://github.com/ezio84/android_external_android-visualizer cm-12.0
+echo -e "${bldgrn}Updating AudioFX by Cyanogen ${txtrst}"
+cd $rdir/packages/apps/AudioFX && git fetch cm && git merge cm/cm-12.0 && git push https://github.com/ezio84/android_packages_apps_AudioFX cm-12.0
+echo -e "${bldgrn}Updating Eleven by Cyanogen ${txtrst}"
+cd $rdir/packages/apps/Eleven && git fetch cm && git merge cm/cm-12.0 && git push https://github.com/ezio84/android_packages_apps_Eleven cm-12.0
 # The end
 echo -e "${bldgrn}Done, exiting ${txtrst}"
