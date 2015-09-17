49f5a809c008b8a39d312681178e6b7599b219ff
ezio84 <brabus84@gmail.com>
2015-09-17 21:59:21 +0200

    manifest upd
    Change-Id: I98f98e6e1bc7793f4daa76b6eb2805c787caeecc

--- fetch-merge-slimroms.sh
+++ fetch-merge-slimroms.sh
@@ -75,8 +75,6 @@ echo -e "${bldgrn}Updating hardware/libhardware ${txtrst}"
 cd $rdir/hardware/libhardware && git checkout lp5.1 && git fetch slimroms && git merge slimroms/lp5.1 && git push https://github.com/ezio84/hardware_libhardware lp5.1
 echo -e "${bldgrn}Updating frameworks/opt/net/wifi ${txtrst}"
 cd $rdir/frameworks/opt/net/wifi && git checkout lp5.1 && git fetch slimroms && git merge slimroms/lp5.1 && git push https://github.com/ezio84/frameworks_opt_net_wifi lp5.1
-echo -e "${bldgrn}Updating packages/apps/KernelAdiutor ${txtrst}"
-cd $rdir/packages/apps/KernelAdiutor && git checkout lp5.1 && git fetch slimsaber && git merge slimsaber/lp5.1 && git push https://github.com/ezio84/KernelAdiutor lp5.1
 echo -e "${bldgrn}Updating device/qcom/common/ ${txtrst}"
 cd $rdir/device/qcom/common && git checkout lp5.1 && git fetch slimroms && git merge slimroms/lp5.1 && git push https://github.com/ezio84/device_qcom_common lp5.1
 #echo -e "${bldgrn}Updating libcore ${txtrst}"
