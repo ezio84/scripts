bc1a363a747118be06ecb5850c87496633fab233
ezio84 <brabus84@gmail.com>
2016-02-11 13:48:48 +0100

    Add nexus vendor fork

--- fetch-merge-purenexus.sh
+++ fetch-merge-purenexus.sh
@@ -7,15 +7,17 @@ rdir=`pwd`
 bldgrn=${txtbld}$(tput setaf 2)
 txtrst=$(tput sgr0)
 
-# warning
+# Warning
 echo -e "${bldgrn}Run this script from the root repo folder! ${txtrst}"
 
-#`check for updates and merge
+# Check for updates and merge
 echo -e "${bldgrn}Updating frameworks/base ${txtrst}"
 cd $rdir/frameworks/base && git checkout mm && git fetch purenexus && git merge purenexus/mm && git push https://github.com/ezio84/purenexus_frameworks_base mm
 echo -e "${bldgrn}Updating packages/apps/Settings ${txtrst}"
 cd $rdir/packages/apps/Settings && git checkout mm && git fetch purenexus && git merge purenexus/mm && git push https://github.com/ezio84/purenexus_packages_apps_Settings mm
 echo -e "${bldgrn}Updating build ${txtrst}"
 cd $rdir/build && git checkout mm && git fetch purenexus && git merge purenexus/mm && git push https://github.com/ezio84/purenexus_build mm
+echo -e "${bldgrn}Updating vendor/nexus/ ${txtrst}"
+cd $rdir/vendor/nexus && git checkout mm && git fetch purenexus && git merge purenexus/mm && git push https://github.com/ezio84/purenexus_vendor_nexus mm
 # The end
 echo -e "${bldgrn}Done, exiting ${txtrst}"
