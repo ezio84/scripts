2bd8ea1436aa6eaa7820f498d6726ba0c287f0de
ezio84 <brabus84@gmail.com>
2016-02-11 16:11:31 +0100

    Add additonal forks

--- fetch-merge-purenexus.sh
+++ fetch-merge-purenexus.sh
@@ -15,6 +15,8 @@ echo -e "${bldgrn}Updating frameworks/base ${txtrst}"
 cd $rdir/frameworks/base && git checkout mm && git fetch purenexus && git merge purenexus/mm && git push https://github.com/ezio84/purenexus_frameworks_base mm
 echo -e "${bldgrn}Updating packages/apps/Settings ${txtrst}"
 cd $rdir/packages/apps/Settings && git checkout mm && git fetch purenexus && git merge purenexus/mm && git push https://github.com/ezio84/purenexus_packages_apps_Settings mm
+echo -e "${bldgrn}Updating packages/apps/PureNexusSettings ${txtrst}"
+cd $rdir/packages/apps/PureNexusSettings && git checkout mm && git fetch purenexus && git merge purenexus/mm && git push https://github.com/ezio84/android_packages_apps_PureNexusSettings
 echo -e "${bldgrn}Updating build ${txtrst}"
 cd $rdir/build && git checkout mm && git fetch purenexus && git merge purenexus/mm && git push https://github.com/ezio84/purenexus_build mm
 echo -e "${bldgrn}Updating vendor/nexus/ ${txtrst}"
