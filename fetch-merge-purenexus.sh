922f69b4a6dba4df01198b44c477b7d5954a041a
ezio84 <brabus84@gmail.com>
2016-02-28 18:50:12 +0100

    new fork

--- fetch-merge-purenexus.sh
+++ fetch-merge-purenexus.sh
@@ -13,6 +13,8 @@ echo -e "${bldgrn}Run this script from the root repo folder! ${txtrst}"
 # Check for updates and merge
 echo -e "${bldgrn}Updating frameworks/base ${txtrst}"
 cd $rdir/frameworks/base && git checkout mm && git fetch purenexus && git merge purenexus/mm && git push https://github.com/ezio84/purenexus_frameworks_base mm
+echo -e "${bldgrn}Updating frameworks/native ${txtrst}"
+cd $rdir/frameworks/native && git checkout mm && git fetch purenexus && git merge purenexus/mm && git push https://github.com/ezio84/purenexus_frameworks_native mm
 echo -e "${bldgrn}Updating packages/apps/Settings ${txtrst}"
 cd $rdir/packages/apps/Settings && git checkout mm && git fetch purenexus && git merge purenexus/mm && git push https://github.com/ezio84/purenexus_packages_apps_Settings mm
 echo -e "${bldgrn}Updating packages/apps/PureNexusSettings ${txtrst}"
