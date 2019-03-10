ae435a9ad152fcf95bc681f2029150e9ca887c40
ezio84 <brabus84@gmail.com>
2019-03-10 10:35:49 +0100

    signed user build tips

---
+++ signed build/generate-keys-for-signed-build.sh
@@ -0,0 +1,29 @@
+openssl genrsa -3 -out temp.pem 2048
+openssl req -new -x509 -key temp.pem -out releasekey.x509.pem -days 10000 -subj '/C=US/ST=California/L=San Narciso/O=Yoyodyne, Inc./OU=Yoyodyne Mobility/CN=Yoyodyne/emailAddress=yoyodyne@example.com'
+openssl pkcs8 -in temp.pem -topk8 -outform DER -out releasekey.pk8 -nocrypt
+shred --remove temp.pem
+
+openssl genrsa -3 -out temp.pem 2048
+openssl req -new -x509 -key temp.pem -out media.x509.pem -days 10000 -subj '/C=US/ST=California/L=San Narciso/O=Yoyodyne, Inc./OU=Yoyodyne Mobility/CN=Yoyodyne/emailAddress=yoyodyne@example.com'
+openssl pkcs8 -in temp.pem -topk8 -outform DER -out media.pk8 -nocrypt
+shred --remove temp.pem
+
+openssl genrsa -3 -out temp.pem 2048
+openssl req -new -x509 -key temp.pem -out platform.x509.pem -days 10000 -subj '/C=US/ST=California/L=San Narciso/O=Yoyodyne, Inc./OU=Yoyodyne Mobility/CN=Yoyodyne/emailAddress=yoyodyne@example.com'
+openssl pkcs8 -in temp.pem -topk8 -outform DER -out platform.pk8 -nocrypt
+shred --remove temp.pem
+
+openssl genrsa -3 -out temp.pem 2048
+openssl req -new -x509 -key temp.pem -out shared.x509.pem -days 10000 -subj '/C=US/ST=California/L=San Narciso/O=Yoyodyne, Inc./OU=Yoyodyne Mobility/CN=Yoyodyne/emailAddress=yoyodyne@example.com'
+openssl pkcs8 -in temp.pem -topk8 -outform DER -out shared.pk8 -nocrypt
+shred --remove temp.pem
+
+openssl genrsa -3 -out temp.pem 2048
+openssl req -new -x509 -key temp.pem -out testkey.x509.pem -days 10000 -subj '/C=US/ST=California/L=San Narciso/O=Yoyodyne, Inc./OU=Yoyodyne Mobility/CN=Yoyodyne/emailAddress=yoyodyne@example.com'
+openssl pkcs8 -in temp.pem -topk8 -outform DER -out testkey.pk8 -nocrypt
+shred --remove temp.pem
+
+openssl genrsa -3 -out temp.pem 2048
+openssl req -new -x509 -key temp.pem -out verity.x509.pem -days 10000 -subj '/C=US/ST=California/L=San Narciso/O=Yoyodyne, Inc./OU=Yoyodyne Mobility/CN=Yoyodyne/emailAddress=yoyodyne@example.com'
+openssl pkcs8 -in temp.pem -topk8 -outform DER -out verity.pk8 -nocrypt
+shred --remove temp.pem
