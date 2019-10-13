b09fef852f3a7acb019a48561be04ff7359b745c
ezio84 <brabus84@gmail.com>
2019-10-13 23:34:27 +0200

    Add network perm needed by Q aosp build

--- signed build/generate-keys-for-signed-build.sh
+++ signed build/generate-keys-for-signed-build.sh
@@ -27,3 +27,8 @@ openssl genrsa -3 -out temp.pem 2048
 openssl req -new -x509 -key temp.pem -out verity.x509.pem -days 10000 -subj '/C=US/ST=California/L=San Narciso/O=Yoyodyne, Inc./OU=Yoyodyne Mobility/CN=Yoyodyne/emailAddress=yoyodyne@example.com'
 openssl pkcs8 -in temp.pem -topk8 -outform DER -out verity.pk8 -nocrypt
 shred --remove temp.pem
+
+openssl genrsa -3 -out temp.pem 2048
+openssl req -new -x509 -key temp.pem -out networkstack.x509.pem -days 10000 -subj '/C=US/ST=California/L=San Narciso/O=Yoyodyne, Inc./OU=Yoyodyne Mobility/CN=Yoyodyne/emailAddress=yoyodyne@example.com'
+openssl pkcs8 -in temp.pem -topk8 -outform DER -out networkstack.pk8 -nocrypt
+shred --remove temp.pem
